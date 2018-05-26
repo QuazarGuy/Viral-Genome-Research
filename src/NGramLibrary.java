cf1b2dfb481f5cf18d93ec4c8c573eb741881225
Jeff Talada <j.talada@bellevuecollege.edu>
2018-05-25 20:06:50 -0700

    Added acidgroup enum and properties

--- src/NGramLibrary.java
+++ src/NGramLibrary.java
@@ -30,4 +30,56 @@ public class NGramLibrary {
 		BPtoAA.put("guu", AminoAcid.V); BPtoAA.put("gua", AminoAcid.V); BPtoAA.put("gua", AminoAcid.V); BPtoAA.put("gug", AminoAcid.V);
 		BPtoAA.put("uaa", AminoAcid.stop); BPtoAA.put("uag", AminoAcid.stop); BPtoAA.put("uga", AminoAcid.stop);
 	}
+	
+	public enum AcidGroup {
+		Na, Ar, Pu, Pc, Nc
+	}
+	
+	static {
+		HashMap<AminoAcid, AcidGroup> AAtoAG = new HashMap<AminoAcid, AcidGroup>();
+		AAtoAG.put(AminoAcid.A, AcidGroup.Na);
+		AAtoAG.put(AminoAcid.R, AcidGroup.Pc);
+		AAtoAG.put(AminoAcid.N, AcidGroup.Pu);
+		AAtoAG.put(AminoAcid.D, AcidGroup.Nc);
+		AAtoAG.put(AminoAcid.C, AcidGroup.Pu);
+		AAtoAG.put(AminoAcid.Q, AcidGroup.Pu);
+		AAtoAG.put(AminoAcid.E, AcidGroup.Nc);
+		AAtoAG.put(AminoAcid.G, AcidGroup.Na);
+		AAtoAG.put(AminoAcid.H, AcidGroup.Pc);
+		AAtoAG.put(AminoAcid.I, AcidGroup.Na);
+		AAtoAG.put(AminoAcid.L, AcidGroup.Na);
+		AAtoAG.put(AminoAcid.K, AcidGroup.Pc);
+		AAtoAG.put(AminoAcid.M, AcidGroup.Na);
+		AAtoAG.put(AminoAcid.F, AcidGroup.Ar);
+		AAtoAG.put(AminoAcid.P, AcidGroup.Na);
+		AAtoAG.put(AminoAcid.S, AcidGroup.Pu);
+		AAtoAG.put(AminoAcid.T, AcidGroup.Pu);
+		AAtoAG.put(AminoAcid.W, AcidGroup.Ar);
+		AAtoAG.put(AminoAcid.Y, AcidGroup.Ar);
+		AAtoAG.put(AminoAcid.V, AcidGroup.Na);
+	}
+	
+	static {
+		HashMap<AminoAcid, Integer> HydropathyIndex = new HashMap<AminoAcid, Integer>();
+		HydropathyIndex.put(AminoAcid.A, 18);
+		HydropathyIndex.put(AminoAcid.R, -45);
+		HydropathyIndex.put(AminoAcid.N, -35);
+		HydropathyIndex.put(AminoAcid.D, -35);
+		HydropathyIndex.put(AminoAcid.C, 25);
+		HydropathyIndex.put(AminoAcid.Q, -35);
+		HydropathyIndex.put(AminoAcid.E, -35);
+		HydropathyIndex.put(AminoAcid.G, -4);
+		HydropathyIndex.put(AminoAcid.H, -32);
+		HydropathyIndex.put(AminoAcid.I, 45);
+		HydropathyIndex.put(AminoAcid.L, 38);
+		HydropathyIndex.put(AminoAcid.K, -39);
+		HydropathyIndex.put(AminoAcid.M, 19);
+		HydropathyIndex.put(AminoAcid.F, 28);
+		HydropathyIndex.put(AminoAcid.P, -16);
+		HydropathyIndex.put(AminoAcid.S, -8);
+		HydropathyIndex.put(AminoAcid.T, -7);
+		HydropathyIndex.put(AminoAcid.W, -9);
+		HydropathyIndex.put(AminoAcid.Y, -13);
+		HydropathyIndex.put(AminoAcid.V, 42);
+	}
 }
